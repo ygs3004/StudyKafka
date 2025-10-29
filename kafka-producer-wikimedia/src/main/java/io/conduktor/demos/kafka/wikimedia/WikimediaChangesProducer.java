5eaae0f642fa065e8ab662a6acbe21375f7f159d
ygs <ygs3004@naver.com>
2025-10-29 22:20:10 +0900

    producer 안정성 옵션

--- kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -2,6 +2,7 @@ package io.conduktor.demos.kafka.wikimedia;
 
 import com.launchdarkly.eventsource.EventHandler;
 import com.launchdarkly.eventsource.EventSource;
+import okhttp3.Headers;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -21,6 +22,11 @@ public class WikimediaChangesProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        // 안정성을 위한 configs(Kafka <= 2.8)
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true"); // 멱등성 처리
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all"); // -1, replica의 데이터 복제처리 안정성
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+
         // Producer 생성
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
@@ -28,7 +34,9 @@ public class WikimediaChangesProducer {
 
         EventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
         String url = "https://stream.wikimedia.org/v2/stream/recentchange";
-        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+
+        Headers headers = Headers.of("User-Agent", "kafka-demo"); // 없을시 403
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url)).headers(headers);
         EventSource eventSource = builder.build();
 
         // 다른 스레드에서 Producer 실행
