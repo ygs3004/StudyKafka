5625b176b1b27fdf40d51b7882d04801e3df1d36
ygs <ygs3004@naver.com>
2025-11-03 19:54:26 +0900

    restHighLevelClient for opensearch consumer

--- kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
@@ -1,7 +1,67 @@
 package io.conduktor.demos.kafka.opensearch;
 
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.client.indices.CreateIndexRequest;
+import org.opensearch.client.indices.GetIndexRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+
 public class OpenSearchConsumer {
-    public static void main(String[] args) {
-        System.out.println("Hello Consumer");
+    public static void main(String[] args) throws IOException {
+        // OpenSearch Client 생성
+        Logger log = LoggerFactory.getLogger(OpenSearchConsumer.class.getSimpleName());
+
+        // OpenSearchClient 생성
+        RestHighLevelClient openSearchClient = createOpenSearchClient();
+
+        // index 미존재시 생성
+        String indexName = "wikimedia";
+        CreateIndexRequest createIndexRequest = new CreateIndexRequest(indexName);
+        try(openSearchClient) {
+
+            boolean indexExists = openSearchClient.indices().exists(new GetIndexRequest(indexName), RequestOptions.DEFAULT);
+            if (!indexExists) {
+                openSearchClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+                log.info("index 생성 완료");
+            }else{
+                log.info("index가 생성 되어있습니다.");
+            }
+        }
     }
+
+    public static RestHighLevelClient createOpenSearchClient() {
+        String connString = "http://localhost:9200";
+
+        RestHighLevelClient restHighLevelClient;
+        URI connUri = URI.create(connString);
+        String userInfo = connUri.getUserInfo();
+
+        if (userInfo == null) {
+            // Rest Client wiout security
+            restHighLevelClient = new RestHighLevelClient(RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), connUri.getScheme())));
+        }else{
+            String[] auth = userInfo.split(":");
+            CredentialsProvider cp = new BasicCredentialsProvider();
+            cp.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(auth[0], auth[1]));
+
+            restHighLevelClient = new RestHighLevelClient(RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), connUri.getScheme()))
+                    .setHttpClientConfigCallback(
+                            httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(cp).setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
+                    ));
+        }
+
+        return restHighLevelClient;
+    }
+
 }
