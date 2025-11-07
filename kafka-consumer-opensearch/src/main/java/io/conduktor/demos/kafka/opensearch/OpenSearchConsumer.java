354d6df640ea69d927cb0f6efc598cf083b43bab
ygs <ygs3004@naver.com>
2025-11-07 18:31:49 +0900

    Consumer BulkRequest

--- kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
@@ -12,6 +12,8 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.opensearch.action.bulk.BulkRequest;
+import org.opensearch.action.bulk.BulkResponse;
 import org.opensearch.action.index.IndexRequest;
 import org.opensearch.action.index.IndexResponse;
 import org.opensearch.client.RequestOptions;
@@ -61,6 +63,8 @@ public class OpenSearchConsumer {
                 int recordCount = records.count();
                 log.info("Received: " + recordCount + " record(s)");
 
+                BulkRequest bulkRequest = new BulkRequest();
+
                 for(ConsumerRecord<String, String> record : records) {
 
                     try{
@@ -72,9 +76,22 @@ public class OpenSearchConsumer {
                         IndexRequest indexRequest = new IndexRequest(indexName)
                                 .source(record.value(), XContentType.JSON)
                                 .id(id);
-                        IndexResponse response = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
-                        log.info(response.getId());
+                        // IndexResponse response = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
+                        // log.info(response.getId());
+                        bulkRequest.add(indexRequest);
+
                     }catch (Exception e){
+                    }
+                }
+
+                if (bulkRequest.numberOfActions() > 0) {
+                    BulkResponse bulkResponse = openSearchClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+                    log.info("Inserted " + bulkResponse.getItems().length + " record(s)");
+
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        log.error(e.getMessage(), e);
                     }
                 }
 
