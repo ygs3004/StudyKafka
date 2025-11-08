6d761004aa2eb876855d7ca91db375675e968a79
ygs <ygs3004@naver.com>
2025-11-08 21:09:13 +0900

    Consumer, OpenSearchClient 종료시 shutdown hook 처리

--- kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
@@ -11,6 +11,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.opensearch.action.bulk.BulkRequest;
 import org.opensearch.action.bulk.BulkResponse;
@@ -41,6 +42,24 @@ public class OpenSearchConsumer {
 
         // KafkaConsumer 생성
         KafkaConsumer<String, String> consumer = createKafkaConsumer();
+
+        // 메인 쓰레드 종료시 shutdown hook 추가
+        final Thread mainThread = Thread.currentThread();
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            @Override
+            public void run() {
+                log.info("Detected a shutdown, by consumer.wakeup()");
+                // 메인 쓰레드의 consumer 객체에 wakeup exception 발생
+                consumer.wakeup();
+
+                // main thread를 감지
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    log.error(e.getMessage());
+                }
+            }
+        });
 
         // index 미존재시 생성
         String indexName = "wikimedia";
@@ -99,7 +118,16 @@ public class OpenSearchConsumer {
                 consumer.commitSync();
                 log.info("Batch Offset Commited!!!");
             }
+        }catch (WakeupException e){
+            log.info("Consumer is starting to shutdown");
+        }catch (Exception e){
+            log.error("Unexpected Exception", e);
+        }finally {
+            consumer.close(); // consumer close, offset commit
+            openSearchClient.close();
+            log.info("The consumer is now gracefully shutdown");
         }
+
     }
 
     private static String extractId(String json){
